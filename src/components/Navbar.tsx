f03610122d38e33968cf369cee740e804bd1f666
Roushan ali <roushanrhaman064@gmail.com>
2024-02-12 02:42:37 +0600

    theme add [done]

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,20 +1,46 @@
 import { Link } from "react-router-dom";
 import Container from "./Container";
+import Theme from "./Theme";
+import { ChangeEvent, useEffect, useState } from "react";
 
 const Navbar = () => {
+  const [theme, setTheme] = useState(
+    localStorage.getItem("theme") ? localStorage.getItem("theme") : "light"
+  );
+
+  // handle theme color change
+  const handleTheme = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.checked) {
+      setTheme("dark");
+    } else {
+      setTheme("light");
+    }
+  };
+
+  useEffect(() => {
+    localStorage.setItem('theme', theme as string)
+    const savedTheme = localStorage.getItem('theme');
+    document.querySelector('html')!.setAttribute('data-theme', savedTheme as string)
+}, [theme])
+
+
   return (
     <div className="w-full bg-white z-10 shadow-sm">
       <div className="py-4 border-b-[1px]">
         <Container>
           <div className="flex justify-between items-center">
-            <div className="flex flex-row items-center justify-between gap-3 md:gap-0">
-              <Link to={"/"} className="text-black text-2xl font-bold">
-                <span className="text-rose-600">Manage</span> User
-              </Link>
-            </div>
+            <Link to={"/"} className="text-black text-2xl font-bold">
+              <span className="text-rose-600">Manage</span> User
+            </Link>
+
             <Link className="text-black text-xl font-semibold" to={"/add-user"}>
               Add User
             </Link>
+
+            <Theme 
+            handleTheme={handleTheme}
+            theme={theme as 'light' | 'dark'}
+            />
           </div>
         </Container>
       </div>
